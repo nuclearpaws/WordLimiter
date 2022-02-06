1964f1194b230a67b80a7a6ddd182275e065b9d2
NuclearPaws <nuclearpaws@gmail.com>
2022-02-06 17:48:16 +0000

    Added help text for the CLI args.

--- src/WordLimiter.Cli/ArgumentOptions/DefaultOptions.cs
+++ src/WordLimiter.Cli/ArgumentOptions/DefaultOptions.cs
@@ -4,10 +4,10 @@ namespace WordLimiter.Cli.ArgumentOptions;
 
 internal sealed class DefaultArgs
 {
-    [Option('l', "length", Required = true)]
+    [Option('l', "length", Required = true, HelpText = "The length of words to be limited by this tool (eg. 5).")]
     public int Length { get; set; }
 
-    [Option('g', "guess", Required = false)]
+    [Option('g', "guess", Required = false, HelpText = "The Wordle like guess represented like \"<word> <feedback>\" where word is the word guess and feedback is \".\" for invalid letter, \"?\" for misplaced letter and \"!\" for correctly guessed letter. Multiples are accepted.")]
     public IEnumerable<string> Guesses { get; set; }
 
     public DefaultArgs()
